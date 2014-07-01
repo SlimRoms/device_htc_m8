22da67567dd7fce87a9b6a97288f14ee5acbae29
Nicholas Flintham <nick@flinny.org>
2014-07-01 13:23:26 +0100

    Revert "Include HTC EasyAcess" Included from vendor as it should have been from the start.
    This reverts commit a505dab06288c3fc71f4701f34bcc68287812a4e.
    
    Change-Id: I5d7affee90449a4f9af48b399bc953af69082239

--- device.mk
+++ device.mk
@@ -85,9 +85,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libaudioamp
 
-PRODUCT_PACKAGES += \
-    EasyAccessService
-
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
