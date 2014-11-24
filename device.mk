2fddac5619d4f640e00cbaa937ed11880e6c82aa
Ethan Chen <intervigil@gmail.com>
2014-11-23 18:33:27 -0600

    m8: add qcmediaplayer
    Change-Id: I8fc52effa67c3d7202ebd3a4d0ddf583499c66a5

--- device.mk
+++ device.mk
@@ -185,7 +185,11 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libstagefrighthw \
-    libdashplayer
+    libdashplayer \
+    qcmediaplayer
+
+PRODUCT_BOOT_JARS += \
+    qcmediaplayer
 
 # Power
 PRODUCT_PACKAGES += \
