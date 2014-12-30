6d7f8337c9330bb0a98489526d92ffa630440b34
Josue Rivera <prbassplayer@gmail.com>
2014-12-29 18:43:07 -0800

    M8: Fixup minor things
      * Device specifics should always be called last.
      * Fix comment
      * Remove call for none existing vendor folder
    
    Change-Id: Id6fbdbe65aae713f9ffa19145cccfda84237a562
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -1,9 +1,10 @@
-$(call inherit-product, device/htc/m8/full_m8.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit common phone config.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Device specifics should always come last
+$(call inherit-product, device/htc/m8/full_m8.mk)
 
 PRODUCT_NAME := slim_m8
